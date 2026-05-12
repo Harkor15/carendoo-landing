39b2f517fb4375a8ee11e1339e92376fa5d414cb
Harkor <krzbigos@gmail.com>
2026-05-13 01:11:43 +0200

    Deploy with new domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: '/carendoo-landing/',
+    base: '/',
     server: {
       port: 3000,
     },
